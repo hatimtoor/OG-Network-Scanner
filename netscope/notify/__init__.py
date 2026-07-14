c731dfb92fe1b83d0f4cd5d6a5c109071c09e7af
hatimtoor <hatimtoor2025@gmail.com>
2026-07-14 14:04:43 +0500

    phaseB(R21): export send_html_email

--- netscope/notify/__init__.py
+++ netscope/notify/__init__.py
@@ -1,4 +1,4 @@
 """Notification backends (desktop, email, webhook)."""
-from .dispatcher import notify
+from .dispatcher import notify, send_html_email
 
-__all__ = ["notify"]
+__all__ = ["notify", "send_html_email"]
